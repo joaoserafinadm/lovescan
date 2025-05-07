f54ec293aa2eaffe04bb9561429499f8befcbaf2
joaoserafinadm <joaoserafin.adm@gmail.com>
2025-05-07 00:55:40 -0300

    feat: credits page

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,9 +1,18 @@
+import Credits from "./Credits";
 import Header from "./Header";
+import Cookie from 'js-cookie'
+import jwt from 'jsonwebtoken'
 
 export default function Layout({ children }) {
+
+  const token = jwt.decode(Cookie.get('auth'))
   return (
     <>
       <Header />
+      {token && (
+
+        <Credits />
+      )}
       <main style={{ paddingTop: '70px' }}>
         {children}
       </main>
