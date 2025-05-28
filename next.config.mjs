e76357c2435c78f9e8a337453057dd3722bcebb3
Akvo-esg <admin@akvo-esg.com.br>
2025-05-28 11:58:22 -0300

    feat: indexPage cards

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
   reactStrictMode: true,
-  
-  // Configuração para resolver conflitos de versão da biblioteca cookie
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Resolve o conflito de versões da biblioteca cookie
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        'cookie': require.resolve('cookie')
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
-
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-
-//   reactStrictMode: true,
-// };
-
-// export default nextConfig;
