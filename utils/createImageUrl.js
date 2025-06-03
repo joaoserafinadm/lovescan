4922361ef7c647849fe000d6bfe9da2255191b8d
joaoserafinadm <joaoserafin.adm@gmail.com>
2025-06-02 23:10:18 -0300

    feat: front sem para empresas

--- utils/createImageUrl.js
+++ utils/createImageUrl.js
@@ -1,33 +1,27 @@
 export const createImageUrl = async (filesList, directory) => {
-    const uploadPromises = filesList.map(elem => 
-        new Promise((resolve) => {
-            const reader = new FileReader()
-            reader.readAsDataURL(elem)
-            reader.onloadend = async () => {
-                const formData = new FormData()
-                formData.append("file", reader.result)
-                formData.append("upload_preset", directory)
+    const uploadPromises = filesList.map(async (elem) => {
+        const formData = new FormData()
+        formData.append("file", elem)
+        formData.append("upload_preset", directory)
 
-                try {
-                    const res = await axios.post('https://api.cloudinary.com/v1_1/joaoserafinadm/image/upload', formData)
-                    resolve({ 
-                        ...elem, 
-                        url: res.data.secure_url,
-                        id: res.data.public_id
-                    })
-                } catch (e) {
-                    resolve({ 
-                        ...elem, 
-                        url: '', 
-                        id: '', 
-                        error: e 
-                    })
-                }
+        try {
+            const res = await axios.post('https://api.cloudinary.com/v1_1/joaoserafinadm/image/upload', formData)
+            return { 
+                ...elem, 
+                url: res.data.secure_url,
+                id: res.data.public_id
             }
-        })
-    )
+        } catch (e) {
+            return { 
+                ...elem, 
+                url: '', 
+                id: '', 
+                error: e 
+            }
+        }
+    })
 
     return Promise.allSettled(uploadPromises).then(results => 
-        results.map(result => result.value)
+        results.map(result => result.status === 'fulfilled' ? result.value : result.reason)
     )
 }
