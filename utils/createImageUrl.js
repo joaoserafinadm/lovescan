3d5d2376bf4c3cfa8971cc419782e3839298a42f
joaoserafinadm <joaoserafin.adm@gmail.com>
2025-06-02 23:24:33 -0300

    feat: front sem para empresas

--- utils/createImageUrl.js
+++ utils/createImageUrl.js
@@ -1,14 +1,11 @@
 import axios from "axios"
 
 export const createImageUrl = (filesList, directory) => new Promise((resolve, reject) => {
-
-    let newList = []
-
-    let iteration = 0
+    let newList = new Array(filesList.length)
+    let completedCount = 0
+    let hasError = false
 
     for (let [index, elem] of filesList.entries()) {
-
-
         const reader = new FileReader()
         if (reader) {
             reader.readAsDataURL(elem)
@@ -17,29 +14,76 @@ export const createImageUrl = (filesList, directory) => new Promise((resolve, re
                 formData.append("file", reader.result)
                 formData.append("upload_preset", directory)
 
-                axios.post('https://api.cloudinary.com/v1_1/joaoserafinadm/image/upload', formData)
-                    .then(res => {
-                        let newData = {
-                            url: res.data.secure_url,
-                            id: res.data.public_id
-                            // id: res.data.public_id.substr(-20, 20)
-                        }
-                        newList.push({ ...elem, ...newData })
-                        iteration++
-                        if (iteration === filesList.length) resolve(newList)
-                    }).catch(e => {
-                        let newData = {
-                            url: '',
-                            id: '',
-                            error: e
-                        }
-                        newList.push({ ...elem, ...newData })
-                        iteration++
-                        if (iteration === filesList.length) reject(newList)
-                    })
+                try {
+                    const res = await axios.post('https://api.cloudinary.com/v1_1/joaoserafinadm/image/upload', formData)
+                    
+                    newList[index] = {
+                        ...elem,
+                        url: res.data.secure_url,
+                        id: res.data.public_id
+                    }
+                } catch (e) {
+                    hasError = true
+                    newList[index] = {
+                        ...elem,
+                        url: '',
+                        id: '',
+                        error: e
+                    }
+                }
+
+                completedCount++
+                if (completedCount === filesList.length) {
+                    hasError ? reject(newList) : resolve(newList)
+                }
             }
         }
-
     }
-
 })
+
+
+// import axios from "axios"
+
+// export const createImageUrl = (filesList, directory) => new Promise((resolve, reject) => {
+
+//     let newList = []
+
+//     let iteration = 0
+
+//     for (let [index, elem] of filesList.entries()) {
+
+
+//         const reader = new FileReader()
+//         if (reader) {
+//             reader.readAsDataURL(elem)
+//             reader.onloadend = async () => {
+//                 const formData = new FormData()
+//                 formData.append("file", reader.result)
+//                 formData.append("upload_preset", directory)
+
+//                 axios.post('https://api.cloudinary.com/v1_1/joaoserafinadm/image/upload', formData)
+//                     .then(res => {
+//                         let newData = {
+//                             url: res.data.secure_url,
+//                             id: res.data.public_id
+//                             // id: res.data.public_id.substr(-20, 20)
+//                         }
+//                         newList.push({ ...elem, ...newData })
+//                         iteration++
+//                         if (iteration === filesList.length) resolve(newList)
+//                     }).catch(e => {
+//                         let newData = {
+//                             url: '',
+//                             id: '',
+//                             error: e
+//                         }
+//                         newList.push({ ...elem, ...newData })
+//                         iteration++
+//                         if (iteration === filesList.length) reject(newList)
+//                     })
+//             }
+//         }
+
+//     }
+
+// })
