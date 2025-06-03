61fbb47fda5a31428af6d272598acacb044bbadf
joaoserafinadm <joaoserafin.adm@gmail.com>
2025-06-02 23:15:27 -0300

    feat: front sem para empresas

--- utils/createImageUrl.js
+++ utils/createImageUrl.js
@@ -1,27 +1,35 @@
-export const createImageUrl = async (filesList, directory) => {
-    const uploadPromises = filesList.map(async (elem) => {
-        const formData = new FormData()
-        formData.append("file", elem)
-        formData.append("upload_preset", directory)
+export const createImageUrl = (filesList, directory) => new Promise((resolve, reject) => {
+    let newList = new Array(filesList.length)
+    let completed = 0
 
-        try {
-            const res = await axios.post('https://api.cloudinary.com/v1_1/joaoserafinadm/image/upload', formData)
-            return { 
-                ...elem, 
-                url: res.data.secure_url,
-                id: res.data.public_id
+    filesList.forEach((elem, index) => {
+        const reader = new FileReader()
+        reader.readAsDataURL(elem)
+        reader.onloadend = async () => {
+            const formData = new FormData()
+            formData.append("file", reader.result)
+            formData.append("upload_preset", directory)
+
+            try {
+                const res = await axios.post('https://api.cloudinary.com/v1_1/joaoserafinadm/image/upload', formData)
+                newList[index] = { 
+                    ...elem, 
+                    url: res.data.secure_url,
+                    id: res.data.public_id
+                }
+            } catch (e) {
+                newList[index] = { 
+                    ...elem, 
+                    url: '', 
+                    id: '', 
+                    error: e 
+                }
             }
-        } catch (e) {
-            return { 
-                ...elem, 
-                url: '', 
-                id: '', 
-                error: e 
+            
+            completed++
+            if (completed === filesList.length) {
+                resolve(newList)
             }
         }
     })
-
-    return Promise.allSettled(uploadPromises).then(results => 
-        results.map(result => result.status === 'fulfilled' ? result.value : result.reason)
-    )
-}
+})
